083335facb67c3f4a8c74e6f6dae8f473a1b70b2
inukares <zelowski@gmail.com>
2022-10-02 12:40:19 +0200

    add ethers lib and demo conneciton to provider

--- fractional/src/App.tsx
+++ fractional/src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import './App.css';
 
+const useMetamask = () => {
+  const [metamask,setMetamask] = useState();
+
+  useEffect(() => {
+
+  })
+}
+
 function App() {
+  const [block,setBlock] = useState();
+
+  useEffect(() => {
+    const connectToMetamask = async() => {
+
+      // TODO: Change this to add etherum to global window object
+      // @ts-ignore
+      const provider = new ethers.providers.Web3Provider(window?.ethereum)
+      let accounts
+      // const provider = new ethers.providers.Web3Provider(window.ethereum)
+      try {
+        accounts = await provider.send("eth_requestAccounts", []);
+      } catch(error) {
+        console.log(error)
+        alert('FAILED TO CONNECT TO WALLET')
+      }
+      const balance = await provider.getBalance(accounts[0]);
+      const block = await provider.getBlockNumber();
+      console.log('last block ', block)
+      debugger;
+      // provider.on("block", (block) => {
+      //   setBlock(block)
+      //   console.log('block numer:', block)
+      // })
+    }
+    connectToMetamask();
+    
+  })
+
+  // const connectToMetamask = async() => {
+  //   //@ts-ignore
+  //   const provider = new ethers.providers.Web3Provider(window?.ethereum)
+  //   console.log('provider')
+
+  //   const block = await provider.getBlockNumber();
+  //   console.log('last block ', block)
+  //   provider.on("block", (block) => {
+  //     setBlock(block)
+  //     console.log('block numer:', block)
+  //   })
+  //   console.log(provider)
+
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >connnnect ro</div>
     </div>
   );
 }
