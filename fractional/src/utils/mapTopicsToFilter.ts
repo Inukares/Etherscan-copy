f94ee8dff128651f962e908c734d6e4fcb6f7c62
inukares <zelowski@gmail.com>
2022-11-09 09:45:38 +0100

    default empty values to null

--- fractional/src/utils/mapTopicsToFilter.ts
+++ fractional/src/utils/mapTopicsToFilter.ts
@@ -1,8 +1,12 @@
 import { ethers } from 'ethers';
 import { Topics } from '../shared/types';
 
-export const mapTopicsToFilter = (topics: Topics): Array<string | null> => {
-  const [transfer_hash, from, to] = topics;
+export const mapTopicsToFilter = (
+  topics: Array<string | null | undefined>
+): Array<string | null> => {
+  const transfer_hash = topics[0] || null;
+  const from = topics[1] || null;
+  const to = topics[2] || null;
   const filter = [transfer_hash];
   if (typeof from === 'string' && from)
     filter.push(ethers.utils.hexZeroPad(from, 32));
